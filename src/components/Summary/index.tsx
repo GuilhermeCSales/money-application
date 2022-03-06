441a25e356490008236ac3a558d930781144ab6e
Guilherme Costa Sales <guicosal@gmail.com>
2022-03-06 20:52:40 -0300

    created summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Incoming</p>
+          <img src={incomeImg} alt="Income" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Spending</p>
+          <img src={outcomeImg} alt="Spending" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}

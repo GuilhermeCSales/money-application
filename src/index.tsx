f4c6d9cec0b5dfdcc34b7bf22dfc51a47852bb5a
Guilherme Costa Sales <guicosal@gmail.com>
2022-03-19 13:14:41 -0300

    transaction listing

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Website freelance",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2022-03-19 13:00:00"),
+        },
+        {
+          id: 2,
+          title: "Rent",
+          type: "withdraw",
+          category: "House",
+          amount: 1100,
+          createdAt: new Date("2022-03-10 11:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
